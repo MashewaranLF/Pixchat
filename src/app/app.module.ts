53d43f456f5f0b09cd686b1e13b04bc13cf599c9
Maheswarnll <tanuki360@hotmail.com>
2016-11-26 16:17:30 +0700

    add main,pix component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,72 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { ForumApp } from './app.component';
+// Pages
+import { AboutPage } from '../pages/about/about';
+import { CommentCreatePage } from '../pages/comment-create/comment-create';
+import { LoginPage } from '../pages/login/login';
+import { ProfilePage } from '../pages/profile/profile';
+import { SignupPage } from '../pages/signup/signup';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ThreadCommentsPage } from '../pages/thread-comments/thread-comments';
+import { ThreadCreatePage } from '../pages/thread-create/thread-create';
+import { ThreadsPage } from '../pages/threads/threads';
+
+
+import { MainCommentsPage } from '../pages/pixchat/main-comments/main-comments';
+import { MainCreatePage } from '../pages/pixchat/main-create/main-create';
+import { MainPage } from '../pages/pixchat/Main/main';
+// Custom components
+import { ThreadComponent } from '../shared/components/thread.component';
+import { UserAvatarComponent } from '../shared/components/user-avatar.component';
+
+import { MainComponent } from '../shared/components/Main/main.component';
+// providers
+import { APP_PROVIDERS } from '../providers/app.providers';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MainComponent,
+    MainPage,
+    MainCommentsPage,
+    MainCreatePage,
+    ForumApp,
+    AboutPage,
+    CommentCreatePage,
+    LoginPage,
+    ProfilePage,
+    SignupPage,
+    TabsPage,
+    ThreadCommentsPage,
+    ThreadCreatePage,
+    ThreadsPage,
+    ThreadComponent,
+    UserAvatarComponent
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(ForumApp),
+    HttpModule,
+    FormsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MainComponent,
+    MainPage,
+    ForumApp,
+    MainCommentsPage,
+    MainCreatePage,
+    AboutPage,
+    CommentCreatePage,
+    LoginPage,
+    ProfilePage,
+    SignupPage,
+    TabsPage,
+    ThreadCommentsPage,
+    ThreadCreatePage,
+    ThreadsPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [APP_PROVIDERS]
 })
 export class AppModule {}
